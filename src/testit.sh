1d60a2e49190040bcd43e7887d8ef71bfb9aad65
epoch <epoch@hacking.allowed.org>
2017-01-04 21:10:56 -0600

    changed the testit.sh script to include multiple objects to test item selection and higher amounts of shapes

--- src/testit.sh
+++ src/testit.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-../tools/obj2hackvr2.pl woman ../meshes/female_basemesh1_2.obj | ./hackvr $USER
+#!/bin/bash
+cat <(../tools/obj2hackvr2.pl woman ../meshes/female_basemesh1_2.obj) \
+ <(echo "woman move 10 0 0") \
+ <(../tools/obj2hackvr2.pl woman2 ../meshes/female_basemesh1_2.obj) \
+ <(echo "woman2 move -10 0 0") \
+ <(../tools/obj2hackvr2.pl woman3 ../meshes/female_basemesh1_2.obj) | ./hackvr $USER
 #../tools/obj2hackvr.pl woman ../meshes/female_basemesh1_2.obj | ./hackvr epoch
