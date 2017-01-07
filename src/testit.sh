7716223ab6f9024300003097d680f498629175be
epoch <epoch@hacking.allowed.org>
2017-01-07 14:37:54 -0600

    made it randomize the order of shapes of models and slowed it down to make it cooler to watch.

--- src/testit.sh
+++ src/testit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cat <(../tools/obj2hackvr2.pl woman ../meshes/female_basemesh1_2.obj) \
+cat <(../tools/obj2hackvr2.pl woman ../meshes/female_basemesh1_2.obj | sort -R) \
  <(echo "woman move 10 0 0") \
- <(../tools/obj2hackvr2.pl woman2 ../meshes/female_basemesh1_2.obj) \
+ <(../tools/obj2hackvr2.pl woman2 ../meshes/female_basemesh1_2.obj | sort -R) \
  <(echo "woman2 move -10 0 0") \
- <(../tools/obj2hackvr2.pl woman3 ../meshes/female_basemesh1_2.obj) | ./hackvr $USER
+ <(../tools/obj2hackvr2.pl woman3 ../meshes/female_basemesh1_2.obj | sort -R) | ./slowcat 10000 | ./hackvr $USER
 #../tools/obj2hackvr.pl woman ../meshes/female_basemesh1_2.obj | ./hackvr epoch
