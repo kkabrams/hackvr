72148bb734c0d5ae1abd44a126f86bc97b180717
epoch <epoch@hacking.allowed.org>
2017-01-15 00:01:51 -0600

    changed a path

--- traceroute_hackvr.sh
+++ traceroute_hackvr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat <(./tools/dat2hackvr.pl world world/world_110m.txt) ./world_camera <(./traceroute_to_dat.sh $1 | cut '-d ' -f2- | awk '{print $2,$1}' | tail -n+2 | ./tools/dat2hackvr.pl route /dev/stdin) /dev/stdin | ./hackvr epoch
+cat <(./tools/dat2hackvr.pl earth earth/world_110m.txt) ./world_camera <(./traceroute_to_dat.sh $1 | cut '-d ' -f2- | awk '{print $2,$1}' | tail -n+2 | ./tools/dat2hackvr.pl route /dev/stdin) /dev/stdin | ./hackvr epoch
