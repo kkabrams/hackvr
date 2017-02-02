d5cd6145b1c1e99ccbd2b48ccb1f02f4a9a635f6
epoch <epoch@hacking.allowed.org>
2017-02-02 02:42:30 -0600

    forgot to remove carriage returns from gopher

--- filebrowser/backend-gopher.sh
+++ filebrowser/backend-gopher.sh
@@ -8,7 +8,7 @@ port=70
 selection=/
 while true;do
 #not sure why this needs to be printed to show up every time.
-  printf '%s\n' "$selection" | ncat "$server" "$port" | tee /dev/stderr | grep -v ^i | cut -f2
+  printf '%s\n' "$selection" | ncat "$server" "$port" | tee /dev/stderr | grep -v ^i | cut -f2 | tr -d '\r'
 ### need to ask user for input in the form of a pop-up window if the selected target was of type 7
   read -r selection
   echo
