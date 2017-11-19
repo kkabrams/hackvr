3d5b222bdaa0a45380f0996ea9b8773592304a03
epoch <epoch@hacking.allowed.org>
2017-11-19 01:46:11 -0600

    fixed IPs_to_triangles.sh to use addshape instead of addtriangle

--- IPs_to_triangle.sh
+++ IPs_to_triangle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-whob | egrep '^IP|^Lat|^Long' | cut '-d ' -f2- | paste - - - | tr '\t' ' ' | awk '{print $1, $3, $2, 0, $3, $2, 0, $3, $2, 0}' | xargs -L1 printf '%s addtriangle %s %s %s %s %s %s %s %s %s\n'
+whob | egrep '^IP|^Lat|^Long' | cut '-d ' -f2- | paste - - - | tr '\t' ' ' | awk '{print $1, $3, $2, 0, $3, $2, 0, $3, $2, 0}' | xargs -L1 printf '%s addshape 3 %s %s %s %s %s %s %s %s %s\n'
