4a080b8f4f061f1c7cbf0d6d996d535dc3f2db35
epoch <epoch@hacking.allowed.org>
2017-12-19 03:55:35 -0600

    make scripts create their own named pipes and attempt to rm them

--- tictactoe/listen.sh
+++ tictactoe/listen.sh
@@ -3,7 +3,17 @@
 #mabe not. might be buggier.
 #cat board p1out | ncat -lp 1050 > p1in &
 #cat board p2out | ncat -lp 1051 > p2in &
-cat board p1out | ncat -lp 1050 > p1in &
-cat board p2out | ncat -lp 1051 > p1in &
+echo listening in port 1050 for player 1 and 1051 for player 2
 
-cat p1in | ./game.sh | tee p1out p2out
+mknod pin p
+mknod p1out p
+mknod p2out p
+
+cat board p1out | ncat -lp 1050 > pin &
+cat board p2out | ncat -lp 1051 > pin &
+
+cat pin | ./game.sh | tee p1out p2out
+
+rm pin
+rm p1out
+rm p2out
