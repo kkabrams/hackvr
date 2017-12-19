ac2f7a61cd4060a617ae012ead020755c0ef4902
epoch <epoch@hacking.allowed.org>
2017-12-19 03:55:05 -0600

    made tictactoe game.sh use the right parts of addshapes because added colors

--- tictactoe/game.sh
+++ tictactoe/game.sh
@@ -18,12 +18,12 @@ stdbuf -oL uniq \
      printf "$user deletegroup %s\n" "$group"
      #printf "$user deletegroup %s\n" "$group" > /dev/stderr
 #need to get the first point of the group clicked and translate the new shape by that much
-     translatex="$(grep "$group" board | grep -v '^#' | tr -s ' ' | cut '-d ' -f4)"
-     translatey="$(grep "$group" board | grep -v '^#' | tr -s ' ' | cut '-d ' -f5)"
+     translatex="$(grep "$group" board | grep -v '^#' | tr -s ' ' | cut '-d ' -f5)"
+     translatey="$(grep "$group" board | grep -v '^#' | tr -s ' ' | cut '-d ' -f6)"
      cat "marker$turn"
      printf "%s move %s %s 2\n" "_marker" "$(expr "$translatex")" "$(expr "$translatey" - 2 )"
      #printf "%s move %s %s 2\n" "_marker" "$(expr "$translatex")" "$(expr "$translatey" - 2 )" >/dev/stderr
-     grep "$group" board | sed "s/$group/_marker2/g"
+     grep "$group" board | sed "s/$group/_marker2/g" #this is to make it still show the square around it.
      printf "%s renamegroup _marker _marker%s%s\n" "$user" "$(date +%s)" "$RANDOM"
     fi
   done
