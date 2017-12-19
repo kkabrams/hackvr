4b109a2449a483a27bf1e863fb30601262b095f0
epoch <epoch@hacking.allowed.org>
2017-12-19 04:57:22 -0600

    added extras I forgot about

---
+++ meshes/grid_floor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat <(for i in $(seq 1 16);do for j in $(seq 1 16);do echo floor addshape 2 $i 0 $j $i 0 $[j+1];echo floor addshape 2 $i 0 $j $[i+1] 0 $j;done; done) <(echo -e "floor scaleup 8\nepoch export floor\nepoch quit\n") | ../src/hackvr_headless epoch | sed "s/\.0*//g"
