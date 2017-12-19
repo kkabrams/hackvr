826eff5bb47181cf38e16c6658c7876a3e0b7183
epoch <epoch@hacking.allowed.org>
2017-12-19 04:04:34 -0600

    fixed offsetshape to start looping in the right spot. oops.

--- tools/offsetshape.sh
+++ tools/offsetshape.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 #objectname addshape color points_in_shape
-echo offsetshape: $1 $2 $3
-awk '{ for(i=4;i<=(4+($4*3));i+=3){$i+='"$1"';$(i+1)+='"$2"';$(i+2)+='"$3"';} print $0;}'
-
+awk '{ for(i=5;i<=(4+($4*3));i+=3){$i+='"$1"';$(i+1)+='"$2"';$(i+2)+='"$3"';} print $0;}'
