74c364b28d3e77d89783828e151a6883a9e093a2
epoch <epoch@hacking.allowed.org>
2017-12-19 03:28:54 -0600

    updated offsetshape.sh to skip over the new color value in addshape

--- tools/offsetshape.sh
+++ tools/offsetshape.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#objectname addshape points_in_shape
+#objectname addshape color points_in_shape
 echo offsetshape: $1 $2 $3
-awk '{ for(i=4;i<=(3+($3*3));i+=3){$i+='"$1"';$(i+1)+='"$2"';$(i+2)+='"$3"';} print $0;}'
+awk '{ for(i=4;i<=(4+($4*3));i+=3){$i+='"$1"';$(i+1)+='"$2"';$(i+2)+='"$3"';} print $0;}'
 
