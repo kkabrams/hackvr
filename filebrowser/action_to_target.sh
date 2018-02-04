bf8ad64cf81ac989a461e69109035653192d2950
epoch <epoch@hacking.allowed.org>
2018-02-03 23:36:50 -0600

    changed filebrowser action_to_target.sh to use USER instead of epoch.

--- filebrowser/action_to_target.sh
+++ filebrowser/action_to_target.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep --line-buffered ^epoch | stdbuf -oL cut '-d ' -f2- | grep --line-buffered ^action | stdbuf -oL cut '-d ' -f2-
+grep --line-buffered ^USER | stdbuf -oL cut '-d ' -f2- | grep --line-buffered ^action | stdbuf -oL cut '-d ' -f2-
