aa12689cf4f5388086dd3d303029582cc81f88ae
epoch <epoch@hacking.allowed.org>
2018-02-04 00:14:30 -0600

    made twirl.sh be pipable into whatever hackvr you want instead of running hackvr by itself

--- twirl.sh
+++ twirl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat <(cat $1 | sed 's/^[^ ]* /mesh /g') <(./makerotate.sh mesh | ./src/slowcat 10000) | ./src/hackvr $USER
+cat <(cat $1 | sed 's/^[^ ]* /mesh /g') <(./makerotate.sh mesh | ./src/slowcat 10000)
