f93f622d9b7c8c7eb25963cf063a6f83d5bb1fa2
epoch <epoch@hacking.allowed.org>
2018-02-04 00:06:05 -0600

    frontend-hackvr now uses a camera.pos file and the name USER instead of epoch.

--- filebrowser/frontend-hackvr.sh
+++ filebrowser/frontend-hackvr.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-stdbuf -oL $1 < p | ./list_to_cubes.sh | ../hackvr epoch | ./action_to_target.sh > p
+#!/bin/bash
+cat camera.pos <(stdbuf -oL $1 < p | ./list_to_cubes.sh) | ../hackvr USER | ./action_to_target.sh > p
