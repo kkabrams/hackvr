cc26a11eec7b0df5a5b4c35ec5cc1ee816556ef8
epoch <epoch@hacking.allowed.org>
2018-02-21 04:06:55 -0600

    made makerotate rotate through all dimensions... eventually.

--- makerotate.sh
+++ makerotate.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 while true;do
- for i in $(seq 0 359);do
-  echo "$1 rotate 0 $i 0"
+ for x in $(seq 0 5 360);do
+  for y in $(seq 0 5 360);do
+   for z in $(seq 0 5 360);do
+    echo "$1 rotate $x $y $z"
+    sleep .0001
+   done
+  done
  done
 done
