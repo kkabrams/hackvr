43e3c0e1478deb3fd76eed35f4a275c66eb73c3b
epoch <epoch@hacking.allowed.org>
2018-03-21 00:52:29 -0500

    update frontent-hackvr.sh to use PATHed hackvr instead of relative

--- filebrowser/frontend-hackvr.sh
+++ filebrowser/frontend-hackvr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat camera.pos <(stdbuf -oL $1 < p | ./list_to_cubes.sh) | ../hackvr USER | ./action_to_target.sh > p
+cat camera.pos <(stdbuf -oL $1 < p | ./list_to_cubes.sh) | hackvr USER | ./action_to_target.sh > p
