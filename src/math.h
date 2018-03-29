3a85a10c8607b3244f4559e692dd2f63a0282467
epoch <epoch@hacking.allowed.org>
2018-03-29 18:26:45 -0500

    renamed a function

--- src/math.h
+++ src/math.h
@@ -14,7 +14,7 @@ c2_t rotate_c2(c2_t p1,c2_t p2,radians dr);
 c3_t rotate_c3_xr(c3_t p1,c3_t p2,radians xr);
 c3_t rotate_c3_yr(c3_t p1,c3_t p2,radians yr);
 c3_t rotate_c3_zr(c3_t p1,c3_t p2,radians zr);
-c3_group_rot_t *get_group_rotation(char *id);
+c3_group_rot_t *get_group_relative(char *id);
 radians d2r(degrees d);
 degrees r2d(radians r);
 radians points_to_angle(c2_t p1,c2_t p2);
