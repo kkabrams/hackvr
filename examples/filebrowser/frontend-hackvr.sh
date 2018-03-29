52dd0d6fd34e3ad8bef1cd9e613c71f28c48c33a
epoch <epoch@hacking.allowed.org>
2018-03-29 18:20:49 -0500

    moved filebrowser stuff to examples dir

--- examples/filebrowser/frontend-hackvr.sh
+++ examples/filebrowser/frontend-hackvr.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ "_$1" = "_" ];then
+ echo "I need an argument for what my backend is." >&2
+ exit 1
+fi
 cat camera.pos <(stdbuf -oL $1 < p | ./list_to_cubes.sh) | hackvr USER | ./action_to_target.sh > p
