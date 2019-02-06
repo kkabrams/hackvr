129b0a46c89321ee4922c8e712adc9f6ce15dd93
epoch <epoch@hacking.allowed.org>
2019-02-05 21:55:39 -0600

    added default font and set makelabel to use it.

--- bin/makelabel.sh
+++ bin/makelabel.sh
@@ -28,8 +28,8 @@ while read -rN 1 c;do
   xoffset=-6
   yoffset="$(printf '%d - %d\n' $yoffset 10 | bc)"
  fi
- if grep "^$name " ${PREFIX}/share/hackvr/font/font.hackvr 2>&1 > /dev/null;then #don't do this shit unless we actually have something to draw. awk in offsetshape bitches when there's nothing.
-   grep "^$name " ${PREFIX}/share/hackvr/font.hackvr \
+ if grep "^$name " ${PREFIX}/share/hackvr/font/default.hackvr 2>&1 > /dev/null;then #don't do this shit unless we actually have something to draw. awk in offsetshape bitches when there's nothing.
+   grep "^$name " ${PREFIX}/share/hackvr/default.hackvr \
     | sed 's/^'"$name"'/'"$target"'/' \
     | offsetshape.sh "$xoffset" "$yoffset" "$zoffset"
  fi
