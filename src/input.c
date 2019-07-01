c5ce69a867a915fc8c6993705ab36c454616319f
epoch <epoch@hacking.allowed.org>
2019-07-01 05:01:47 -0500

    added a input handler since x11 mouse and kb events come through the same fd

---
+++ src/input.c
@@ -0,0 +1,18 @@
+#include <idc.h>
+#include "common.h"
+#include "graphics_x11.h"
+#include "keyboard_x11.h"
+#include "mouse_x11.h"
+
+extern struct x11_global x11_global;
+
+int input_init() {
+  return x11_global.fd;
+}
+
+void input_event_handler(struct shit *me,char *line) {
+  if(keyboard_event_handler(me,line)) {
+  }
+  if(mouse_event_handler(me,line)) {
+  }
+}
