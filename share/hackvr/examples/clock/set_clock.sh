78dc60ef3859a70582c8def1fc33c003997da9b7
epoch <epoch@hacking.allowed.org>
2020-02-13 16:42:08 -0600

    the hour hand placement is now based on minutes too

--- share/hackvr/examples/clock/set_clock.sh
+++ share/hackvr/examples/clock/set_clock.sh
@@ -2,6 +2,6 @@
 hour=$(date +%H | sed 's/^0//')
 minute=$(date +%M | sed 's/^0//')
 second=$(date +%S | sed 's/^0//')
-printf "_clock_face_hand_hour_ rotate 0 0 -%s\n" "$[hour * 30]"
-printf "_clock_face_hand_minute_ rotate 0 0 -%s\n" "$[minute * 6]"
-printf "_clock_face_hand_second_ rotate 0 0 -%s\n" "$[second * 6]"
+printf "_clock_face_hand_hour_ rotate 0 0 -%s\n" "$[$hour * 30 + ($minute / 2)]"
+printf "_clock_face_hand_minute_ rotate 0 0 -%s\n" "$[$minute * 6]"
+printf "_clock_face_hand_second_ rotate 0 0 -%s\n" "$[$second * 6]"
