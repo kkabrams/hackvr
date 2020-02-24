2fb350627ccb27cea9400da54e3c2dac0919211d
epoch <epoch@hacking.allowed.org>
2020-02-24 05:01:24 -0600

    example script of using the doors in the dungeon

--- share/hackvr/examples/dungen/dunexplore.sh
+++ share/hackvr/examples/dungen/dunexplore.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ### run me with hackvr_coproc
 #the $USER triangle we add first is to give us some sort of avatar to see where the camera is.
-seed=1337
+#seed=1337
 echo $USER addshape 4 3  -1 0 -1  0 0 2  1 0 -1 ; echo $USER move 0 2 0
 ./dungen $seed | ./dun2hackvr
 while read group action target;do
@@ -10,28 +10,26 @@ while read group action target;do
       printf "# clicked a door! %s\n" "${target}" >&2
       if printf "%s\n" "${target}" | grep ^door_open;then
         derp=$(printf "%s\n" "${target}" | sed 's/_open/_CLOSING/')
-        printf "%s renamegroup %s %s\n" ${target} ${target} ${tmp} | tee /dev/stderr
-        while true;do
+        printf "%s renamegroup %s %s\n" ${target} ${target} ${tmp} 
+        (
         for i in $(seq 1 5 90);do
           printf "%s rotate 0 +5 0\n" ${tmp}
           sleep .1
         done
         tmp2=$(printf "%s\n" "${target}" | sed 's/_open/_close/')
-        printf "%s renamegroup %s %s\n" ${tmp} ${tmp} ${tmp2} | tee /dev/stderr
-        break
-        done | tee /dev/stderr &
+        printf "%s renamegroup %s %s\n" ${tmp} ${tmp} ${tmp2}
+        ) &
       else
         tmp=$(printf "%s\n" "${target}" | sed 's/_close/_OPENING/')
-        printf "%s renamegroup %s %s\n" ${target} ${target} ${tmp} | tee /dev/stderr
-        while true;do
+        printf "%s renamegroup %s %s\n" ${target} ${target} ${tmp}
+        (
         for i in $(seq 1 5 90);do
           printf "%s rotate 0 +-5 0\n" ${tmp}
           sleep .1
         done
         tmp2=$(printf "%s\n" "${target}" | sed 's/_close/_open/')
-        printf "%s renamegroup %s %s\n" ${tmp} ${tmp} ${tmp2} | tee /dev/stderr
-        break
-        done | tee /dev/stderr &
+        printf "%s renamegroup %s %s\n" ${tmp} ${tmp} ${tmp2}
+        ) &
       fi
     fi 
   fi
