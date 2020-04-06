f43bc654684239198fc793df536319bd73a38d7f
epoch <epoch@thebackupbox.net>
2020-04-06 06:04:22 -0500

    added an exec

--- share/hackvr/examples/hackvr_term/hackvr_data_decode.sh
+++ share/hackvr/examples/hackvr_term/hackvr_data_decode.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep --line-buffered '^[^ ]* data ' | stdbuf -oL tr -s ' ' | stdbuf -oL cut '-d ' -f3- | stdbuf -o0 xxd -r -p
+exec grep --line-buffered '^[^ ]* data ' | stdbuf -oL tr -s ' ' | stdbuf -oL cut '-d ' -f3- | stdbuf -o0 xxd -r -p
