0999adf305ad0cc27fb793eb3b230751194e4850
epoch <epoch@thebackupbox.net>
2020-10-09 23:59:13 -0500

    added ability to export drawn thing in draw.sh to hard-coded path. moved squares to be centered around their own 0s

--- share/hackvr/examples/draw/draw.sh
+++ share/hackvr/examples/draw/draw.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
-echo "$USER move 0 0 -100"
+echo "$USER move 5 7 -19"
 
-for y in $(seq 1 1 48);do
- for x in $(seq 1 1 48);do
-  echo ${x}_${y} addshape 2 4 $x $y 0 $[x] $[y+1] 0 $[x+1] $[y+1] 0 $[x+1] $[y] 0
+for y in $(seq 0 1 15);do
+ for x in $(seq 0 1 15);do
+  echo cell_${x}_${y} addshape 2 4 -.5 -.5 0 -.5 .5 0 .5 .5 0 .5 -.5 0
+  #echo cell_${x}_${y} addshape 2 4 $x $y 0 $[x] $[y+1] 0 $[x+1] $[y+1] 0 $[x+1] $[y] 0
+  echo cell_${x}_${y} move $x $y 0
  done
 done
 
 ### color pallete
-### 0-8 are ansi colors. +16 to make them not affected by "lighting"
-for c in $(seq 0 8);do
-  echo color_${c} addshape $[c+16] 4 -2 ${c+2} 0 -1 ${c+2} 0 -1 $[c+3] 0 -2 $[c+3] 0
+### 0-8 are ansi colors.
+for c in $(seq 0 16);do
+	echo color_${c} addshape $[c] 4   $[-2 - ($c / 8)] $[($c%8)+3] 0   $[-1 - ($c / 8)] $[($c%8)+3] 0   $[-1 - ($c / 8)] $[($c%8)+4] 0   $[-2 - ($c / 8)] $[($c%8)+4] 0
 done
 
-echo "derp" >&2
+#cat ${PREFIX}/share/hackvr/meshes/floppy.hackvr | grep '^[^ #]' | sed 's/^[^ ][^ ]*/save/g'
+#echo save move -5 0 0
 
-color=3
-echo color_${color} addshape $[color+16] 4 -2 0 0 -2 1 0 -1 1 0 -1 0 0
-
-while read source command target;do
-  printf "target: %s command: %s\n" "$target" "$command" >&2
+tee /dev/stderr | while read -r source command target;do
   if [ "$command" = "action" ];then
+    if echo "$target" | grep "^save" 2>&1 >/dev/null;then
+      echo "$USER export *"
+      date=$(date +%s)
+      echo "$USER ping $date"
+      while read -r line;do
+        if [ "$line" = "${USER} pong ${date}" ];then
+          break
+        else
+          printf "%s\n" "$line" >> ~/drawsave.hackvr
+	fi
+      done
+    fi
     if echo "$target" | grep "^color_" 2>&1 >/dev/null;then
       color=$(echo $target | cut -d_ -f2)
       echo deletegroup $target
-      echo color_${color}_current addshape $[color+16] 4 -2 -2 0 -2 -1 0 -1 -1 0 -1 -2 0
-    else 
-      echo "derp" >&2
-      x=$(echo $target | cut -d_ -f1)
-      y=$(echo $target | cut -d_ -f2)
+      echo color_${color}_current addshape $[$color] 4   -2 0 0 -2 1 0 -1 1 0 -1 0 0
+    elif echo "$target" | grep '^cell_' 2>&1 >/dev/null;then
+      x=$(echo $target | cut -d_ -f2)
+      y=$(echo $target | cut -d_ -f3)
       echo $target deletegroup $target
-      echo $target addshape $[color + 16] 4 $x $y 0 $x $[y+1] 0 $[x+1] $[y+1] 0 $[x+1] $y 0 | tee /dev/stderr
+      #echo $target addshape $[$color] 4 $x $y 0 $x $[y+1] 0 $[x+1] $[y+1] 0 $[x+1] $y 0 | tee /dev/stderr
+      echo $target addshape $[$color] 4 -.5 -.5 0 -.5 .5 0 .5 .5 0 .5 -.5 0
+      printf "%s move %d %d 0" "$target" "$x" "$y"
     fi
   fi
 done
